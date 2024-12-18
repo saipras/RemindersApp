b56d3edf45f633216d51ba4a683defb6cd87e95d
sai-prasad-rekha <saiprasad.rekha@nov.com>
2024-12-18 21:51:31 +0530

    test7

--- RemindersApp/ContentView.swift
+++ RemindersApp/ContentView.swift
@@ -17,16 +17,9 @@ struct ContentView: View {
             Text("Hello, world!")
             Text("Hello, world!")
             Text("Hello, world!")
-
             Text("Hello, world!")
             Text("Hello, world!")
-            
-            
-            Text("Hello, world!")
-            Text("Sai Prasad")
-            Text("Sai Prasad")
-            Text("Sai Prasad")
-            Text("feature/remtest-01")
+        
 
             
             
